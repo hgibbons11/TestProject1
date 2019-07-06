71f74f5581068a7d4acf680780ce068bc8ff8c4f
hgibbons11 <44006155+hgibbons11@users.noreply.github.com>
2019-07-06 11:34:12 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,55 +1,32 @@
 ﻿using System;
 
-namespace TestProject1
+namespace Test2
 {
     class Program
-    { 
-        
-    static void OddSort(int[] arr, int n)
     {
-        
-        int l = 0, r = n - 1;
-
-        // Count of odd numbers 
-        int k = 0;
-
-        while (l < r)
+        static void Main(string[] args)
         {
-            // work through array to identify odd # and even #
-            while (arr[l] % 2 != 0)
+            string conversionWords = "Hello world today will be amazing";
+            string[] sentence = conversionWords.Split(' ');
+
+            
+            string translation = "";
+            string beginningLetter;
+            string wordBody;
+            string space = " ";
+            
+
+            foreach (string word in conversionWords.Split())
             {
-                l++;
-                k++;
-            }
-            while (arr[r] % 2 == 0 && l < r)
-                r--;
+                beginningLetter = word.Substring(0, 1);
+                wordBody = word.Substring(1, word.Length - 1);
+                translation = translation + wordBody + beginningLetter + "ay" + space;
 
-            //sort the numbers
-            if (l < r)
-            {
-                // swap arr[l] arr[r] 
-                int temp = arr[l];
-                arr[l] = arr[r];
-                arr[r] = temp;
 
             }
 
+            Console.WriteLine(translation.ToString());
+            
         }
-
-            // put odd numbers in ascending order 
-            Array.Sort(arr, 0, k);
     }
-
-    public static void Main(String[] args)
-    {
-        int[] arr = { 5, 3, 2, 8, 1, 4 };
-
-        OddSort(arr, arr.Length);
-
-        Console.WriteLine(String.Join(" ", arr));
-    }
-}
-    
-
-    
 }
